23af224fdcb690781925536cc13b8eade497571b
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-06-29 10:38:19 -0600

    Navbar, Program and About working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import About from "./components/About/About";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title
+          subTitle="Our PROGRAM"
+          title="What We Offer"
+        />
+        <Programs />
+        <About />
+      </div>
+    </div>
+  );
 };
 export default App;
